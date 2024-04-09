66a6d21c80917906efb67f11382eef8bed408589
yashrajxchopra <yashrajxchopra@gmail.com>
2024-04-09 22:01:46 +0530

    Remove Comments

--- Server/index.js
+++ Server/index.js
@@ -66,12 +66,12 @@ const User = mongoose.model('User', UserSchema);
 // Multer storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/') // Destination folder for uploaded files
+    cb(null, 'uploads/') 
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + generateRandomString(); // Generate unique suffix
-    const originalExtension = path.extname(file.originalname); // Get original file extension
-    const uniqueFileName = file.fieldname + '-' + uniqueSuffix + originalExtension; // Construct unique filename
+    const uniqueSuffix = Date.now() + '-' + generateRandomString(); 
+    const originalExtension = path.extname(file.originalname); 
+    const uniqueFileName = file.fieldname + '-' + uniqueSuffix + originalExtension; 
     cb(null, uniqueFileName);
   }
 });
@@ -80,7 +80,7 @@ const upload = multer({ storage: storage });
 
 const PostSchema = new mongoose.Schema({
     caption: { type: String},
-    createdBy: { type: String, required: true }, // Reference to the User who made the post
+    createdBy: { type: String, required: true }, 
     image: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     comments: [{
@@ -95,13 +95,10 @@ const Post = mongoose.model('Posts', PostSchema);
 
 app.post('/api/register', async (req, res) => {
   try {
-    // Extract data from request body
     const { username, password, email, fullName, bio } = req.body;
-
-    // Hash the password
+    
     const hashedPassword = await bcrypt.hash(password, 10); // Salt rounds = 10
 
-    // Create a new User instance with hashed password
     const newUser = new User({
       username,
       password: hashedPassword,
@@ -110,13 +107,10 @@ app.post('/api/register', async (req, res) => {
       bio
     });
 
-    // Save the user to the database
     await newUser.save();
 
-    // Respond with success message
     res.status(201).json({ message: 'User created successfully', user: newUser });
   } catch (err) {
-    // If there's an error, respond with error message
     res.status(500).json({ error: err.message });
   }
   });
@@ -125,12 +119,10 @@ app.post('/api/register', async (req, res) => {
 app.post('/api/login', async (req, res) => {
     try {
       const { email, password } = req.body;
-      // Find the user by email
       const user = await User.findOne({ email });
       if (!user) {
         return res.status(404).json({ error: 'User not found' });
       }
-      // Compare the provided password with the hashed password stored in the database
       const passwordMatch = await bcrypt.compare(password, user.password);
       if (!passwordMatch) {
         return res.status(401).json({ error: 'Invalid password' });
@@ -146,7 +138,6 @@ app.post('/api/login', async (req, res) => {
 app.post('/api/posts',authenticateToken, upload.single('image'), async (req, res) => {
   try {
     if (!req.file) {
-      // If no file was uploaded, respond with an error message
       return res.status(400).json({ error: 'No file uploaded' });
     }
     const { caption, createdBy, image, comments, likes } = req.body;
@@ -169,31 +160,27 @@ app.post('/api/posts',authenticateToken, upload.single('image'), async (req, res
     }
     user.posts.push(newPost._id);
     await user.save();
+
     res.status(200).json({ message: 'Post added successfully', post: newPost });
     
-
-    // Respond with success message
     res.status(201).json({ message: 'Post created successfully', post: newPost });
   } catch (err) {
-    // If there's an error, respond with error message
     res.status(500).json({ error: err.message });
   }
   });
+  //like the post
   app.post('/api/posts/:postId/like', async (req, res) => {
     try {
       const postId = req.params.postId;
   
-      // Find the post by ID
       const post = await Post.findById(postId);
   
       if (!post) {
         return res.status(404).json({ error: 'Post not found' });
       }
   
-      // Add the user's ID to the likes array
-      post.likes.push(req.body.userId); // Assuming you have userId in the request body
+      post.likes.push(req.body.userId); 
   
-      // Save the updated post
       await post.save();
   
       res.status(200).json({ message: 'Like added successfully', post });
@@ -203,23 +190,20 @@ app.post('/api/posts',authenticateToken, upload.single('image'), async (req, res
     }
   });
   
-  // Route to add a comment to a post
+  // add a comment to a post
   app.post('/api/posts/:postId/comment', async (req, res) => {
     try {
       const postId = req.params.postId;
       const { comment, createdBy } = req.body;
   
-      // Find the post by ID
       const post = await Post.findById(postId);
   
       if (!post) {
         return res.status(404).json({ error: 'Post not found' });
       }
   
-      // Add the new comment to the comments array
       post.comments.push({ comment, createdBy, createdAt: Date.now() });
   
-      // Save the updated post
       await post.save();
   
       res.status(200).json({ message: 'Comment added successfully', post });
@@ -231,7 +215,7 @@ app.post('/api/posts',authenticateToken, upload.single('image'), async (req, res
 
 app.get('/api/posts', async (req, res) => {
     try {
-      const posts = await Post.find().exec(); // Find all posts
+      const posts = await Post.find().exec(); /
       res.status(200).json(posts);
     } catch (error) {
       res.status(500).json({ error: error.message });
